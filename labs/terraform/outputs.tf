78f1204f08bce9aa1a439c175d67b03cb92b094a
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2026-03-02 19:24:52 +0000

    lab 03: register outputs

---
+++ labs/terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "resource_group_id" {
+description = "The ID of the resource group"
+value       = azurerm_resource_group.main.id
+}
+
+output "vnet_id" {
+description = "The ID of the virtual network"
+value       = azurerm_virtual_network.main.id
+}
+
+output "vnet_address_space" {
+description = "The address space of the virtual network"
+value       = azurerm_virtual_network.main.address_space
+}
