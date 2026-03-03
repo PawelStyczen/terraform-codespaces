d0e11bc9484977339aa188537883bf32047b5ff7
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2026-03-03 13:15:26 +0000

    added outputs

--- labs/terraform/outputs.tf
+++ labs/terraform/outputs.tf
@@ -17,3 +17,23 @@ output "vnet_location" {
   description = "The location of the virtual network"
   value       = azurerm_virtual_network.main.location
 }
+
+output "web_subnet_id" {
+  description = "The ID of the web subnet"
+  value       = azurerm_subnet.web.id
+}
+
+output "app_subnet_id" {
+  description = "The ID of the app subnet"
+  value       = azurerm_subnet.app.id
+}
+
+output "web_nsg_id" {
+  description = "The ID of the web network security group"
+  value       = azurerm_network_security_group.web.id
+}
+
+output "app_nsg_id" {
+  description = "The ID of the app network security group"
+  value       = azurerm_network_security_group.app.id
+}
