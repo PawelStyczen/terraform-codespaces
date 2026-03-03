f9b83ef00d3caa27e82311b1fbbf2ee8f962a914
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2026-03-03 13:09:25 +0000

    LAB 03: nsg associations added

--- labs/terraform/main.tf
+++ labs/terraform/main.tf
@@ -107,4 +107,12 @@ resource "azurerm_network_security_rule" "app_internal" {
   destination_address_prefix = "*"
 }
 
-
+#NSG ASSOCIATIONS
+resource "azurerm_subnet_network_security_group_association" "web" {
+  subnet_id                 = azurerm_subnet.web.id
+  network_security_group_id = azurerm_network_security_group.web.id
+}
+resource "azurerm_subnet_network_security_group_association" "app" {
+  subnet_id                 = azurerm_subnet.app.id
+  network_security_group_id = azurerm_network_security_group.app.id
+}
